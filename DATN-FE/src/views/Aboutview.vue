f68acfad73fbd997be8262c910db2b728e3b04b3
Nguyễn Hữu Huấn <huan.nguyen2@kiaisoft.com>
2024-01-05 17:44:35 +0700

    about view

--- DATN-FE/src/views/Aboutview.vue
+++ DATN-FE/src/views/Aboutview.vue
@@ -1,52 +1,207 @@
 <template>
-    <Navbar />
-    <section id="about">
-        <div class="container mt-5 pt-5">
-            <h3 class="pt-5">About </h3>
-            <hr>
-            <div class="row flex-column-reverse flex-lg-row mx-auto ">
-                <div class="col-lg-6">
-                    <h4>This Website Create By <a class="website_link" target="_blank"
-                            href="https://webdev531.000webhostapp.com/">Nguyễn Trung Kiên</a></h4>
-                    <p> Hello everyone!</p>
-                    <p>🎉 Exciting news! I've just released an of rymo shopping website, packed with new features and
-                        improvements. Here are some of the highlights:</p>
-                    <p>📱 Version - 1.0</p>
-                    <p>✨ What's new - Login with Google, Phone Number, Facebook</p>
-                    <p>💻 Technology & framework: Sass-less, Vue 3 JS, CSS3, Firebase (Authentication).</p>
-                    <p>📒 Note-taking feature: You can easily take purchase product functionality after that login or
-                        signing up.</p>
-                    <p>🔥 UI Credit: "Tech2 etc" (YouTube channel).</p>
-                    <p>⚡ Performance improvements: The Website now loads faster, and there's less lag when switching between
-                        screens.</p>
-                    <p>I'm working to improve the web application and provide the best user experience possible. I hope you
-                        enjoy using these features and improvements as much as I've enjoyed developing them.</p>
-                    <p>I have Good understanding of HTML5 and CSS3. Good Knowledge with the JavaScript language and its
-                        modern ES6+ syntax and features. Experience with Vue.js framework and its core principles such as
-                        components, reactivity, and the virtual DOM. Familiarity with Vuex, Vue Router.</p>
-                    <p>I am highly skilled in HTML, CSS, and JavaScript as I have handsome experience in them. Still, I am
-                        always eager to learn new and make my skill sharper whenever I get any chance.</p>
-                </div>
-                <div class="col-lg-6">
-                    <div class="developer_img pb-3">
-                        <a target="_blank" href="https://www.linkedin.com/in/yash-borda-8abb9124b/">
-                            <img class="img-fluid" src="../assets/img/yash_borda.jpg" alt="yash borda">
-                        </a>
-                    </div>
-                </div>
+  <Navbar />
+  <section id="about">
+    <div class="container mt-5 pt-5">
+      <h3 class="pt-5">About</h3>
+      <hr />
+      <div class="txt-top-banner">
+        <h2>ABOUT US</h2>
+
+        <p>
+          <strong>THE FASHION STORE</strong> caters to thoughtful shoppers who
+          appreciate unique designs and <a>top quality</a> pieces you just can’t
+          find anywhere else. We are constantly curating fresh new collections
+          and looking for the next big thing our customers will love. Founded in
+          <span>Vienna</span> in 2019, we are proud to be your
+          <span>Online Clothing Shop</span> that you can rely on for our expert
+          service and care.
+        </p>
+      </div>
+      <div class="sec-full-col" style="height: auto !important">
+        <div class="container" style="height: auto !important">
+          <div class="txt-third-full">
+            <div class="txt-third-bg thr-col11">
+              <div class="txt-third-img">
+                <img src="@/assets/img/shirt_500x500.png" />
+              </div>
+              <strong>Our Mission </strong>is to make a difference through our
+              branding by staying ahead of the fashion trends, defining style
+              and giving customers what they want.
             </div>
+
+            <div class="txt-third-bg thr-col22">
+              <div class="txt-third-img">
+                <img src="@/assets/img/henger_500x500.png" />
+              </div>
+              <strong>Our Vision</strong> is to change the way our society
+              connects with the fashion industry by providing our customers with
+              products that are ethically and responsibly sourced.
+            </div>
+          </div>
+          <div
+            class="google-auto-placed"
+            style="width: 100%; height: auto; clear: both; text-align: center"
+          >
+            <ins
+              data-ad-format="auto"
+              class="adsbygoogle adsbygoogle-noablate"
+              data-ad-client="ca-pub-4973858419289911"
+              data-adsbygoogle-status="done"
+              style="
+                display: block;
+                margin: 40px auto 10px;
+                background-color: transparent;
+                height: 0px;
+              "
+              data-ad-status="unfilled"
+              ><div
+                id="aswift_2_host"
+                tabindex="0"
+                title="Advertisement"
+                aria-label="Advertisement"
+                style="
+                  border: none;
+                  height: 0px;
+                  width: 1110px;
+                  margin: 0px;
+                  padding: 0px;
+                  position: relative;
+                  visibility: visible;
+                  background-color: transparent;
+                  display: inline-block;
+                  overflow: hidden;
+                  opacity: 0;
+                "
+              >
+                <iframe
+                  id="aswift_2"
+                  name="aswift_2"
+                  browsingtopics="true"
+                  style="
+                    left: 0px;
+                    position: absolute;
+                    top: 0px;
+                    border: 0px;
+                    width: 1110px;
+                    height: 0px;
+                  "
+                  sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+                  width="1110"
+                  height="0"
+                  frameborder="0"
+                  marginwidth="0"
+                  marginheight="0"
+                  vspace="0"
+                  hspace="0"
+                  allowtransparency="true"
+                  scrolling="no"
+                  allow="attribution-reporting"
+                  src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-4973858419289911&amp;output=html&amp;h=280&amp;adk=2740529114&amp;adf=418819730&amp;pi=t.aa~a.417841341~rp.4&amp;w=1110&amp;fwrn=4&amp;fwrnh=100&amp;lmt=1704439851&amp;rafmt=1&amp;to=qs&amp;pwprc=3233295376&amp;format=1110x280&amp;url=https%3A%2F%2Fwww.thefashion-store.com%2Fpages%2Fabout-us&amp;ea=0&amp;fwr=0&amp;pra=3&amp;rpe=1&amp;resp_fmts=3&amp;wgl=1&amp;fa=40&amp;uach=WyJXaW5kb3dzIiwiMTUuMC4wIiwieDg2IiwiIiwiMTIwLjAuNjA5OS4xMzEiLG51bGwsMCxudWxsLCI2NCIsW1siTm90X0EgQnJhbmQiLCI4LjAuMC4wIl0sWyJDaHJvbWl1bSIsIjEyMC4wLjYwOTkuMTMxIl0sWyJHb29nbGUgQ2hyb21lIiwiMTIwLjAuNjA5OS4xMzEiXV0sMF0.&amp;dt=1704439851118&amp;bpp=1&amp;bdt=1289&amp;idt=-M&amp;shv=r20240103&amp;mjsv=m202401030101&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;prev_fmts=0x0%2C1200x280&amp;nras=3&amp;correlator=1882743106246&amp;frm=20&amp;pv=1&amp;ga_vid=1812791988.1704439851&amp;ga_sid=1704439851&amp;ga_hid=859064704&amp;ga_fc=1&amp;u_tz=420&amp;u_his=6&amp;u_h=1080&amp;u_w=1920&amp;u_ah=1032&amp;u_aw=1920&amp;u_cd=24&amp;u_sd=1&amp;dmc=8&amp;adx=397&amp;ady=2181&amp;biw=1903&amp;bih=945&amp;scr_x=0&amp;scr_y=0&amp;eid=44759876%2C44759927%2C44759837%2C44785292%2C44809005%2C31080235%2C31078663%2C31078665%2C31078668%2C31078670&amp;oid=2&amp;pvsid=2951515042800785&amp;tmod=1052789634&amp;uas=0&amp;nvt=1&amp;ref=https%3A%2F%2Fwww.google.com%2F&amp;fc=1920&amp;brdim=0%2C0%2C0%2C0%2C1920%2C0%2C1920%2C1032%2C1920%2C945&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=128&amp;bc=31&amp;td=1&amp;psd=W251bGwsbnVsbCxudWxsLDFd&amp;nt=1&amp;ifi=3&amp;uci=a!3&amp;btvi=2&amp;fsb=1&amp;dtd=16"
+                  data-google-container-id="a!3"
+                  data-google-query-id="CNi99p7dxYMDFWfNFgUdCNQBaA"
+                  data-load-complete="true"
+                ></iframe></div
+            ></ins>
+          </div>
+
+          <div class="serve-you-we">
+            <div class="serve-you-we-logo">
+              <img
+                src="//www.thefashion-store.com/cdn/shop/files/ezgif.com-gif-maker_52_500x500.jpg?v=1613237622"
+              />
+            </div>
+
+            <div class="serve-you-we-txt">
+              <p>
+                <strong>THE FASHION STORE</strong> is here to serve you and make
+                sure you find the <span>Perfect Look</span> for every occasion.
+                Our passion for fashion is the reason why we are here.
+                <span>We absolutely love what we do,</span> and our goal is to
+                help our customers by providing them with
+                <span>unique outfit and accessories </span>that make them
+                stand-out from the crowd.
+              </p>
+            </div>
+          </div>
         </div>
-    </section>
-    <Footer />
+      </div>
+    </div>
+  </section>
+  <Footer />
 </template>
-  
+
 <script>
-import Footer from '@/components/Footer.vue';
-import Navbar from '@/components/Navbar.vue';
+import Footer from "@/components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-    name: "AboutView",
-    components: { Navbar, Footer }
-}
+  name: "AboutView",
+  components: { Navbar, Footer },
+};
 </script>
+<style scoped>
+.txt-top-banner {
+  text-align: center;
+  bottom: -315px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 73%;
+  padding: 55px 125px 70px;
+  border: 1px solid #bc9979;
+  background-color: #fff;
+  margin-bottom: 180px;
+}
 
+.txt-top-banner h2 {
+  font-weight: 700;
+  font-family: Poppins, sans-serif;
+  text-transform: uppercase;
+  font-size: 40px;
+  line-height: 40px;
+  margin-bottom: 32px;
+  color: #bc9979;
+}
+
+.txt-third-full {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  width: 89%;
+  margin: 22px auto 0;
+}
+
+.txt-third-bg {
+  border: 1px solid #bc9979;
+  padding: 25px 38px;
+  margin: 0 auto;
+  width: 50%;
+  font-family: Poppins, sans-serif;
+  font-size: 18px;
+  color: #000;
+  font-weight: 200;
+  line-height: 30px;
+}
+
+.txt-third-img {
+  margin-bottom: 23px;
+}
+
+.txt-third-full .txt-third-bg.thr-col22 {
+  margin-left: 10px;
+}
+
+.serve-you-we {
+  width: 89%;
+  margin: 40px auto 60px;
+  background-color: #bc9979;
+  padding: 50px 23px 36px 20px;
+  display: flex;
+  align-items: center;
+}
+
+.serve-you-we .serve-you-we-logo {
+  width: 100%;
+  margin-right: 34px;
+}
+</style>
