3bcf4185c198f5882b6691bdc1ac71643b9581e5
Nguyen Huu Huan <gundamakp01@gmail.com>
2024-01-15 23:57:37 +0700

    build

---
+++ DATN-FE/dist/assets/LoginService.28e9aeef.js
@@ -0,0 +1,1 @@
+import{A as o}from"./index.85923ff2.js";const e={login(r){return o.post("login",r)},logout(){return o.post("logout")}};export{e as L};
---
+++ DATN-FE/dist/assets/logo.d15f1ef9.js
@@ -0,0 +1,1 @@
+const s="/assets/logo.a8a5b7b9.png";export{s as _};
