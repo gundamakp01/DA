105d1b62bdd5173b266ed2066bd9f9fd764011ba
Nguyễn Hữu Huấn <huan.nguyen2@kiaisoft.com>
2024-01-16 17:41:58 +0700

    17:41 16/1

---
+++ DATN-FE/dist/assets/DistrictService.45c02f8a.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index.86d05ebb.js";const i={getProvinces(){return t.get("provinces")}},s={getWardByDistrictId(r){return t.get(`districts/${r}/wards`)}},c={getDistrictByProvince(r){return t.get(`provinces/${r}/districts`)}};export{c as D,i as P,s as W};
