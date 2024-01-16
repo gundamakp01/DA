e28ba7f426f5341f5879f47c65795057be4a18a8
Nguyen Huu Huan <gundamakp01@gmail.com>
2024-01-16 22:33:08 +0700

    build 10:33 16/1

--- DATN-FE/dist/assets/OrderService.78b28eb4.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index.86a833dd.js";const s={getOrder(){return e.get("orders")},createOrder(r){return e.post("orders",r)},deleteOrder(r){return e.delete(`orders/${r}`)},updateOrder(r,t){return e.update(`orders/${r}`,t)},getOrderById(r){return e.get(`orders/${r}`)},changeStatus(r){return e.post("orders/change-status",r)}};export{s as O};
