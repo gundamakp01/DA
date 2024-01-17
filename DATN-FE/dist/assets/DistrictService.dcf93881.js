00422c60f365bc035bcdd6bc1b3877f006670401
Nguyen Huu Huan <gundamakp01@gmail.com>
2024-01-17 23:53:05 +0700

    23:32 17/1

---
+++ DATN-FE/dist/assets/DistrictService.dcf93881.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index.8ca39612.js";const i={getProvinces(){return t.get("provinces")}},s={getWardByDistrictId(r){return t.get(`districts/${r}/wards`)}},c={getDistrictByProvince(r){return t.get(`provinces/${r}/districts`)}};export{c as D,i as P,s as W};
