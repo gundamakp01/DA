94969fa9354299ba12240e9b49b845842f26c0d6
Nguyen Huu Huan <huan.nguyen2@kiaisoft.com>
2024-01-18 11:10:00 +0700

    fix vnpay

--- Eva/src/app/Http/Controllers/UserController.php
+++ Eva/src/app/Http/Controllers/UserController.php
@@ -84,19 +84,20 @@ class UserController extends Controller
      */
     public function signup(SignUpRequest $request)
     {
-        return $this->userRepository->create($request->only('name', 'email', 'phone_number', 'password','province_id', 'district_id', 'ward_id'));
+        return $this->userRepository->create($request->only('name', 'email', 'phone_number', 'password', 'province_id', 'district_id', 'ward_id'));
     }
 
     public function editProfile(Request $request)
     {
-        return $this->userRepository->update($request->only('name', 'phone_number','province_id', 'district_id', 'ward_id'), auth()->id());
+        return $this->userRepository->update($request->only('name', 'phone_number', 'province_id', 'district_id', 'ward_id'), auth()->id());
     }
 
-    public function payment(Request $request) {
+    public function payment(Request $request)
+    {
         $order = $this->orderRepository->findWithSum($request->id);
         $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        $vnp_Returnurl = "http://localhost:5173";
-        $vnp_TmnCode = "24UAIXK5";//Mã website tại VNPAY
+        $vnp_Returnurl = "http://3.225.88.245";
+        $vnp_TmnCode = "24UAIXK5"; //Mã website tại VNPAY
         $vnp_HashSecret = "QPMYIBZROQSBGDVXUSMHFRIHRELGSZNW"; //Chuỗi bí mật
 
         $vnp_TxnRef = $order->id;
@@ -106,29 +107,29 @@ class UserController extends Controller
         $vnp_Locale = 'vn';
         $vnp_BankCode = "NCB";
         $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
-//Add Params of 2.0.1 Version
-//        $vnp_ExpireDate = $_POST['txtexpire'];
-//Billing
-//        $vnp_Bill_Mobile = $_POST['txt_billing_mobile'];
-//        $vnp_Bill_Email = $_POST['txt_billing_email'];
-//        $fullName = trim($_POST['txt_billing_fullname']);
-//        if (isset($fullName) && trim($fullName) != '') {
-//            $name = explode(' ', $fullName);
-//            $vnp_Bill_FirstName = array_shift($name);
-//            $vnp_Bill_LastName = array_pop($name);
-//        }
-//        $vnp_Bill_Address=$_POST['txt_inv_addr1'];
-//        $vnp_Bill_City=$_POST['txt_bill_city'];
-//        $vnp_Bill_Country=$_POST['txt_bill_country'];
-//        $vnp_Bill_State=$_POST['txt_bill_state'];
-//// Invoice
-//        $vnp_Inv_Phone=$_POST['txt_inv_mobile'];
-//        $vnp_Inv_Email=$_POST['txt_inv_email'];
-//        $vnp_Inv_Customer=$_POST['txt_inv_customer'];
-//        $vnp_Inv_Address=$_POST['txt_inv_addr1'];
-//        $vnp_Inv_Company=$_POST['txt_inv_company'];
-//        $vnp_Inv_Taxcode=$_POST['txt_inv_taxcode'];
-//        $vnp_Inv_Type=$_POST['cbo_inv_type'];
+        //Add Params of 2.0.1 Version
+        //        $vnp_ExpireDate = $_POST['txtexpire'];
+        //Billing
+        //        $vnp_Bill_Mobile = $_POST['txt_billing_mobile'];
+        //        $vnp_Bill_Email = $_POST['txt_billing_email'];
+        //        $fullName = trim($_POST['txt_billing_fullname']);
+        //        if (isset($fullName) && trim($fullName) != '') {
+        //            $name = explode(' ', $fullName);
+        //            $vnp_Bill_FirstName = array_shift($name);
+        //            $vnp_Bill_LastName = array_pop($name);
+        //        }
+        //        $vnp_Bill_Address=$_POST['txt_inv_addr1'];
+        //        $vnp_Bill_City=$_POST['txt_bill_city'];
+        //        $vnp_Bill_Country=$_POST['txt_bill_country'];
+        //        $vnp_Bill_State=$_POST['txt_bill_state'];
+        //// Invoice
+        //        $vnp_Inv_Phone=$_POST['txt_inv_mobile'];
+        //        $vnp_Inv_Email=$_POST['txt_inv_email'];
+        //        $vnp_Inv_Customer=$_POST['txt_inv_customer'];
+        //        $vnp_Inv_Address=$_POST['txt_inv_addr1'];
+        //        $vnp_Inv_Company=$_POST['txt_inv_company'];
+        //        $vnp_Inv_Taxcode=$_POST['txt_inv_taxcode'];
+        //        $vnp_Inv_Type=$_POST['cbo_inv_type'];
         $inputData = array(
             "vnp_Version" => "2.1.0",
             "vnp_TmnCode" => $vnp_TmnCode,
@@ -142,21 +143,21 @@ class UserController extends Controller
             "vnp_OrderType" => $vnp_OrderType,
             "vnp_ReturnUrl" => $vnp_Returnurl,
             "vnp_TxnRef" => $vnp_TxnRef,
-//            "vnp_ExpireDate"=>$vnp_ExpireDate,
-//            "vnp_Bill_Mobile"=>$vnp_Bill_Mobile,
-//            "vnp_Bill_Email"=>$vnp_Bill_Email,
-//            "vnp_Bill_FirstName"=>$vnp_Bill_FirstName,
-//            "vnp_Bill_LastName"=>$vnp_Bill_LastName,
-//            "vnp_Bill_Address"=>$vnp_Bill_Address,
-//            "vnp_Bill_City"=>$vnp_Bill_City,
-//            "vnp_Bill_Country"=>$vnp_Bill_Country,
-//            "vnp_Inv_Phone"=>$vnp_Inv_Phone,
-//            "vnp_Inv_Email"=>$vnp_Inv_Email,
-//            "vnp_Inv_Customer"=>$vnp_Inv_Customer,
-//            "vnp_Inv_Address"=>$vnp_Inv_Address,
-//            "vnp_Inv_Company"=>$vnp_Inv_Company,
-//            "vnp_Inv_Taxcode"=>$vnp_Inv_Taxcode,
-//            "vnp_Inv_Type"=>$vnp_Inv_Type
+            //            "vnp_ExpireDate"=>$vnp_ExpireDate,
+            //            "vnp_Bill_Mobile"=>$vnp_Bill_Mobile,
+            //            "vnp_Bill_Email"=>$vnp_Bill_Email,
+            //            "vnp_Bill_FirstName"=>$vnp_Bill_FirstName,
+            //            "vnp_Bill_LastName"=>$vnp_Bill_LastName,
+            //            "vnp_Bill_Address"=>$vnp_Bill_Address,
+            //            "vnp_Bill_City"=>$vnp_Bill_City,
+            //            "vnp_Bill_Country"=>$vnp_Bill_Country,
+            //            "vnp_Inv_Phone"=>$vnp_Inv_Phone,
+            //            "vnp_Inv_Email"=>$vnp_Inv_Email,
+            //            "vnp_Inv_Customer"=>$vnp_Inv_Customer,
+            //            "vnp_Inv_Address"=>$vnp_Inv_Address,
+            //            "vnp_Inv_Company"=>$vnp_Inv_Company,
+            //            "vnp_Inv_Taxcode"=>$vnp_Inv_Taxcode,
+            //            "vnp_Inv_Type"=>$vnp_Inv_Type
         );
 
         if (isset($vnp_BankCode) && $vnp_BankCode != "") {
@@ -166,7 +167,7 @@ class UserController extends Controller
             $inputData['vnp_Bill_State'] = $vnp_Bill_State;
         }
 
-//var_dump($inputData);
+        //var_dump($inputData);
         ksort($inputData);
         $query = "";
         $i = 0;
@@ -183,12 +184,12 @@ class UserController extends Controller
 
         $vnp_Url = $vnp_Url . "?" . $query;
         if (isset($vnp_HashSecret)) {
-            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//
+            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret); //
             $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
         }
-        $returnData = array('code' => '00'
-        , 'message' => 'success'
-        , 'data' => $vnp_Url);
+        $returnData = array(
+            'code' => '00', 'message' => 'success', 'data' => $vnp_Url
+        );
         if (isset($_POST['redirect'])) {
             header('Location: ' . $vnp_Url);
             die();
